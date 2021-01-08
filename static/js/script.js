4dc2d4149c013646b3be101656e33094a931bc84
Yassinrby <yassinrby@outlook.com>
2021-01-08 15:29:52 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,5 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-     
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+});
